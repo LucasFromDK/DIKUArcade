cd12ce8f1b2e0af7ef4c460b69fb6b7cd8966d35
LucasFromDK <lucashjerbechlarsen@gmail.com>
2025-03-20 19:54:38 +0100

    SoundPlayer Documentation

--- DIKUArcade/Sound/SoundPlayer.cs
+++ DIKUArcade/Sound/SoundPlayer.cs
@@ -5,19 +5,33 @@ using System.Media;
 public class CustomSoundPlayer {
     private SoundPlayer player;
 
+    /// <summary>
+    /// Constructor for CustomSoundPlayer.
+    /// </summary>
+    /// <param name="soundFilePath">
+    /// The path to the sound file that should be played.
+    /// </param>
     public CustomSoundPlayer(string soundFilePath) {
-        // Create a System.Media.SoundPlayer
         player = new System.Media.SoundPlayer(soundFilePath);
     }
 
+    /// <summary>
+    /// Play the sound once.
+    /// </summary>
     public void Play() {
         player.Play();
     }
 
+    /// <summary>
+    /// Play the sound in a loop.
+    /// </summary>
     public void PlayLooping() {
         player.PlayLooping();
     }
 
+    /// <summary>
+    /// Stop the sound.
+    /// </summary>
     public void Stop() {
         player.Stop();
     }
